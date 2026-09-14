0c03ae57ba88dfa6a51ef71ec5e78b674f5df369
nika :3 <thanikadecarpevitam@gmail.com>
2026-09-14 12:33:14 -0400

    Changed to while loop

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,7 +7,8 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        int i = 1;
+        while (i <= 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +32,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i += 1;
         }
     }
 }
